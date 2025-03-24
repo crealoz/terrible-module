d72fff85027a4eed7a3e595f1093732c75bb4b63
Christophe Ferreboeuf <christophe@crealoz.fr>
2025-03-24 09:41:39 +0100

    add some terrible stuff

--- registration.php
+++ registration.php
@@ -1,3 +1,13 @@
+/**
+ * EasyAudit Premium - Magento 2 Audit Extension
+ *
+ * Copyright (c) 2025 Crealoz. All rights reserved.
+ * Licensed under the EasyAudit Premium EULA.
+ *
+ * This software is provided under a paid license and may not be redistributed,
+ * modified, or reverse-engineered without explicit permission.
+ * See EULA for details: https://crealoz.fr/easyaudit-eula
+ */
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
