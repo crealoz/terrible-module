5edd75d36e9cf52b21680b30898542a242370fe9
Christophe Ferreboeuf <christophe@crealoz.fr>
2025-09-25 15:35:19 +0200

    remove useless faulty comments

--- Model/ResourceModel/Faq/Collection.php
+++ Model/ResourceModel/Faq/Collection.php
@@ -1,13 +1,4 @@
-/**
- * EasyAudit Premium - Magento 2 Audit Extension
- *
- * Copyright (c) 2025 Crealoz. All rights reserved.
- * Licensed under the EasyAudit Premium EULA.
- *
- * This software is provided under a paid license and may not be redistributed,
- * modified, or reverse-engineered without explicit permission.
- * See EULA for details: https://crealoz.fr/easyaudit-eula
- */
+
 <?php
 
 namespace Crealoz\TerribleModule\Model\ResourceModel\Faq;
