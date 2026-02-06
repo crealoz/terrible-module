46401e9d60c34da1c94950da4d1108c73abd338b
Christophe Ferreboeuf <christophe@crealoz.fr>
2026-02-06 12:15:30 +0100

    making things worse

--- Helper/FaqList.php
+++ Helper/FaqList.php
@@ -11,11 +11,13 @@ class FaqList extends \Magento\Framework\App\Helper\AbstractHelper
     public function __construct(
         CollectionFactory $faqCollectionFactory
     ) {
+        $this->objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $this->faqCollectionFactory = $faqCollectionFactory;
     }
 
     public function getFaqCollection()
     {
+        $user = $this->objectManager->get('Magento\User\Model\User');
         $collection = $this->faqCollectionFactory->create();
         $collection->addFieldToFilter('is_active', 1);
         return $collection;
